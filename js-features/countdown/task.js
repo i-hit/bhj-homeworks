ea437ebcd5c5058c497ef3e0f60bdf43a5a780e5
Andrey Semenov <andss82@gmail.com>
2020-07-21 18:14:44 +0300

    task 1 done

---
+++ js-features/countdown/task.js
@@ -0,0 +1,34 @@
+const elementTimer = document.getElementById("timer");
+
+// timer in minutes
+const time = 0.1; 
+
+function ticTac(time) {
+  let allSeconds = time * 60;
+
+  function updateTicTac() {
+    allSeconds--;
+    let seconds = allSeconds % 60;
+    let minutes = allSeconds / 60 % 60;
+    let hours = allSeconds / 60 / 60 % 24;
+
+    let result = new Date(0, 0, 0, hours, minutes, seconds).toLocaleTimeString();
+    
+    elementTimer.textContent = result;
+
+    if (allSeconds < 0) {
+      clearInterval(timeInterval);
+      document.getElementById("status").textContent = "Конкурс завершен";
+      alert("Вы победили в конкурсе!");
+      document.getElementById("win").href = "https://speed.hetzner.de/100MB.bin";
+      document.getElementById("win").click();
+    }
+  }
+
+  updateTicTac();
+  let timeInterval = setInterval(updateTicTac, 1000);
+}
+
+ticTac(time);
+
+
