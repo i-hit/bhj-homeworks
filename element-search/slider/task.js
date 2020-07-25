e0b700cd225d19d3641e0a1891a723a53d9c862e
Andrey Semenov <andss82@gmail.com>
2020-07-25 17:42:07 +0300

    task 3 extra done

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,21 +1,31 @@
 const next = document.querySelector(".slider__arrow_next");
 const prev = document.querySelector(".slider__arrow_prev");
 const slides = document.querySelectorAll(".slider__item")
-const classActive = "slider__item_active";
+const dots = document.querySelectorAll(".slider__dot");
+
 let currentSlide = 0;
 
 next.onclick = function() {
-  toggleSlideClass();
+  toggleSlide();
   currentSlide + 1 === slides.length ? currentSlide = 0 : currentSlide++;
-  toggleSlideClass();
+  toggleSlide();
 }
 
 prev.onclick = function() {
-  toggleSlideClass();
+  toggleSlide();
   currentSlide - 1 < 0 ? currentSlide = slides.length - 1 : currentSlide--;
-  toggleSlideClass();
+  toggleSlide();
 }
 
-function toggleSlideClass() {
-  slides[currentSlide].classList.toggle(classActive);
+for (let i = 0; i < dots.length; i++) {
+  dots[i].onclick = function() {
+    toggleSlide();
+    currentSlide = i;
+    toggleSlide();
+  }
+}
+
+function toggleSlide() {
+  slides[currentSlide].classList.toggle("slider__item_active");
+  dots[currentSlide].classList.toggle("slider__dot_active");
 }
