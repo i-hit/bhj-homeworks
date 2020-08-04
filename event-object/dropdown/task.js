cb025ad0a3897094e0a4c96a2176b00342c625e1
Andrey Semenov <andss82@gmail.com>
2020-08-04 11:13:04 +0300

    task 1 format files

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -2,24 +2,31 @@ const dropdownLists = document.querySelectorAll(".dropdown__list");
 const dropdownValues = document.querySelectorAll(".dropdown__value");
 const dropdownItems = document.querySelectorAll(".dropdown__item");
 
-dropdownValues.forEach((el) => el.addEventListener("click", (e) => {
-  toggleList(e.currentTarget);
-}))
+dropdownValues.forEach((el) =>
+  el.addEventListener("click", (e) => {
+    toggleList(e.currentTarget, "dropdown__list_active");
+  })
+);
 
-dropdownItems.forEach((el) => el.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log(el.textContent);
-  el.closest(".dropdown").querySelector(".dropdown__value").textContent = el.textContent;
-  dropdownLists.forEach((el) => el.classList.remove("dropdown__list_active"));
-}
-))
+dropdownItems.forEach((el) =>
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    el.closest(".dropdown").querySelector(".dropdown__value").textContent =
+      el.textContent;
+    removeClass(dropdownLists, "dropdown__list_active");
+  })
+);
 
-function toggleList(el) {
+function toggleList(el, className) {
   const nextEl = el.nextElementSibling;
-  if (nextEl.classList.contains("dropdown__list_active")) {
-    nextEl.classList.remove("dropdown__list_active");
+  if (nextEl.classList.contains(className)) {
+    nextEl.classList.remove(className);
   } else {
-    dropdownLists.forEach((el) => el.classList.remove("dropdown__list_active"));
+    removeClass(dropdownLists, className);
     nextEl.classList.add("dropdown__list_active");
   }
-} 
+}
+
+function removeClass(el, className) {
+  el.forEach((el) => el.classList.remove(className));
+}
