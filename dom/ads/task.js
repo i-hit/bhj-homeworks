71aaaedfdf0308bda45f5c813b7ca11369bccbb9
Andrey Semenov <andss82@gmail.com>
2020-08-08 23:59:06 +0300

    task 2 add some rotators

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,35 +1,51 @@
 "use strict";
 
 const parents = document.querySelectorAll(".rotator");
-const ads = Array.from(document.querySelectorAll(".rotator__case"));
-let i = ads.length - 1;
-let time = +ads[0].dataset.speed;
-let color = ads[0].dataset.color;
 
-startTimer();
+parents.forEach((e) => {
 
-function changeTime() {
-  time = +ads[i].dataset.speed;
-}
-
-function changeColor() {
-  color = ads[i].dataset.color;
-}
-
-function startTimer() {
-  ads[i].classList.remove("rotator__case_active");
-  i++;
-
-  if (i === ads.length) {
-    i = 0;
+  const ads = Array.from(e.querySelectorAll(".rotator__case"));
+  let i = ads.length - 1;
+  let time = +ads[0].dataset.speed;
+  let color = ads[0].dataset.color;
+  let fontSize = ads[0].dataset.fontSize;
+  
+  startTimer();
+  
+  function changeTime() {
+    time = +ads[i].dataset.speed;
+  }
+  
+  function changeColor() {
+    color = ads[i].dataset.color;
   }
 
-  changeColor();
+  function changeFontSize() {
+    if (!ads[i].dataset.fontSize) {
+      fontSize = "1rem";
+    } else {
+      fontSize = ads[i].dataset.fontSize;
+    }
+  }
+  
+  function startTimer() {
+    ads[i].classList.remove("rotator__case_active");
+    i++;
+  
+    if (i === ads.length) {
+      i = 0;
+    }
+  
+    changeColor();
+    changeFontSize();
+  
+    ads[i].style.color = color;
+    ads[i].style.fontSize = fontSize;
+    ads[i].classList.add("rotator__case_active");
+  
+    changeTime();
+  
+    setTimeout(startTimer, time);
+  }
+})
 
-  ads[i].style.color = color;
-  ads[i].classList.add("rotator__case_active");
-
-  changeTime();
-
-  setTimeout(startTimer, time);
-}
