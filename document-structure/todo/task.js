387480469cf187e7771c4ec9e25a85a9dc721abf
Andrey Semenov <andss82@gmail.com>
2020-08-15 02:22:45 +0300

    task 2 fixed, add button remove all tasks

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,43 +2,45 @@
 
 const toDo = document.getElementById("tasks__list");
 const dilerToDo = document.getElementById("task__input");
-const button = document.getElementById("tasks__add");
-let removeToDo;
+const buttonAdd = document.getElementById("tasks__add");
+const buttonRemove = document.querySelector(".tasks__remove");
 
-button.addEventListener("click", (e) => {
+buttonAdd.addEventListener("click", (e) => {
   e.preventDefault();
   if (dilerToDo.value.length) {
     addTask();
-    getRemoveToDo();
   }
 });
 
+buttonRemove.addEventListener("click", () => {
+  toDo.innerHTML = "";
+});
+
 dilerToDo.addEventListener("keydown", (e) => {
-  if (dilerToDo.value.length) {
-    if (e.key === "Enter") {
+  if (e.key === "Enter") {
+    if (dilerToDo.value.length) {
       addTask();
-      getRemoveToDo();
     }
   }
 });
 
-function addTask() {
-  toDo.innerHTML += `
-  <div class="task">
-  <div class="task__title">
-  ${dilerToDo.value}
-  </div>
-  <a href="#" class="task__remove">&times;</a>
-  </div>`;
-  dilerToDo.value = "";
-}
+toDo.addEventListener("click", (e) => {
+  if (e.target.classList.contains("task__remove")) {
+    e.preventDefault();
+    e.target.closest(".task").remove();
+  }
+});
 
-function getRemoveToDo() {
-  removeToDo = document.querySelectorAll(".task__remove");
-  removeToDo.forEach((el) =>
-    el.addEventListener("click", (e) => {
-      e.preventDefault();
-      e.target.closest(".task").remove();
-    })
+function addTask() {
+  toDo.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class="task">
+    <div class="task__title">
+    ${dilerToDo.value}
+    </div>
+    <a href="#" class="task__remove">&times;</a>
+    </div>`
   );
+  dilerToDo.value = "";
 }
