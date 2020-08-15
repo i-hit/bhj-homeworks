01e1a3a617bf42e07abeade3e39fbd3633bfee7f
Andrey Semenov <andss82@gmail.com>
2020-08-15 09:55:45 +0300

    task 1 done extra

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,11 +1,47 @@
 "use strict";
 
 const list = document.getElementById("items");
+const loader = document.getElementById("loader");
+
+if (localStorage.getItem("history")) {
+  const historyList = JSON.parse(localStorage.getItem("history")).response
+    .Valute;
+  loader.classList.remove("loader_active");
+  getList(historyList);
+}
 
 let xhr = new XMLHttpRequest();
 
 xhr.open("GET", "https://netology-slow-rest.herokuapp.com");
 xhr.send();
 
-if (xhr.status === 200) {
+xhr.addEventListener("readystatechange", () => {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    loader.classList.remove("loader_active");
+    const answer = JSON.parse(xhr.responseText).response.Valute;
+    list.innerHTML = "";
+    getList(answer);
+    localStorage.setItem("history", xhr.responseText);
+  }
+});
+
+function getList(element) {
+  for (let key in element) {
+    list.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="item">
+      <div class="item__code">
+      ${element[key].CharCode}
+      </div>
+      <div class="item__value">
+      ${element[key].Value}
+      </div>
+      <div class="item__currency">
+      руб.
+      </div>
+      </div>
+      `
+    );
+  }
 }
